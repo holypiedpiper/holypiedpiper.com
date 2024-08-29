d7376973941dce9f69857b59f2b688162d64fa84
holypied Piper <kreampiedpiper@gmail.com>
2024-08-29 16:42:24 -0400

    Create prop for injecting chosen head component in root layout to pave way for Next.js specific optimization

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -4,14 +4,16 @@ export default function RootLayout({
   googleAnalytics,
   heapIo,
   children,
+  HeadComponent = ({ children }) => <head>{children}</head>,
 }: {
   googleAnalytics: React.ReactNode;
   heapIo: React.ReactNode;
   children: React.ReactNode;
+  HeadComponent?: React.ComponentType<{ children: React.ReactNode }>;
 }) {
   return (
     <html lang="en">
-      <head>
+      <HeadComponent>
         <link rel="icon" type="image/svg+xml" href="/sweat-droplets.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
@@ -23,7 +25,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         />
-      </head>
+      </HeadComponent>
       {googleAnalytics}
       {heapIo}
       <body>
